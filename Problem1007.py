19929dcdaa68c1368d9e34d81e9b721594d70d53
Ahnaf Habib Azfar <ahnafh600@gmail.com>
2020-09-29 23:35:12 +0600

    URI_Problem_Beginner

---
+++ Problem1007.py
@@ -0,0 +1,14 @@
+#Difference
+A = input("")
+B = input("")
+C = input("")
+D = input("")
+
+a = int(A)
+b = int(B)
+c = int(C)
+d = int(D)
+
+DIFERENCA = ((a*b)-(c*d))
+
+print("DIFERENCA =",DIFERENCA, end="\n")
