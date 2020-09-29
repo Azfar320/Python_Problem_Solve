508cd3753b77ad3e861225ddae02d82511f7058c
Ahnaf Habib Azfar <ahnafh600@gmail.com>
2020-09-29 23:36:50 +0600

    URI_Problem_Beginner

---
+++ Problem1008.py
@@ -0,0 +1,13 @@
+Number = int(input(""))
+Work_hours = int(input(""))
+amount = float(input(""))
+
+
+Salary = float(Work_hours*amount)
+
+
+print("NUMBER = %d"%Number,"\nSALARY = U$ %.2f"%Salary)
+
+
+
+
