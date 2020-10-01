15cab24afe925cd9122db580302720000960c1a2
Ahnaf Habib Azfar <ahnafh600@gmail.com>
2020-10-01 15:34:05 +0600

    URI_Problem_Beginner

---
+++ Problem1008.py
@@ -0,0 +1,11 @@
+#SALARY
+
+Employee_Number = int(input(""))
+Employee_worktime_in_month = int(input(""))
+Payment_per_hour = float(input(""))
+
+
+SALARY = (Employee_worktime_in_month*Payment_per_hour)
+
+print("NUMBER = %d"%Employee_Number)
+print("SALARY = U$ %.2f"%SALARY)
